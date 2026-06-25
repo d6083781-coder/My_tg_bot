1f0e0dd77ea3f531159971272089ca65542a36cf
d6083781-coder <d6083781@gmail.com>
2026-06-25 04:03:36 +0300

    Add files via upload

---
+++ 1as.py
@@ -0,0 +1,55 @@
+import asyncio
+import random
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+
+TOKEN = "8834936159:AAGuck2qylYB5vV7iOXSrhTHCapscaOYUKI"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+def get_stars_reward():
+    return random.choices([25, 50, 100], weights=[80, 15, 5])[0]
+
+
+@dp.message()
+async def casino_handler(message: Message):
+    # ❗ проверяем что это dice вообще
+    if not message.dice:
+        return
+
+    # ❗ проверяем emoji
+    if message.dice.emoji != "🎰":
+        return
+
+    value = message.dice.value
+
+    print("DICE:", value)  # <-- ДЛЯ ПРОВЕРКИ В КОНСОЛИ
+
+    if value == 64:
+        msg = await message.reply("🎰 7️⃣7️⃣7️⃣ ДЖЕКПОТ!")
+
+        await asyncio.sleep(1)
+        await msg.edit_text("🎡 Крутим...")
+
+        await asyncio.sleep(1)
+
+        stars = get_stars_reward()
+
+        await msg.edit_text(
+            f"🎰 7️⃣7️⃣7️⃣ ДЖЕКПОТ!\n"
+            f"🌟 +{stars} ⭐"
+        )
+    else:
+        await message.reply("🎰 Не повезло 😢")
+
+
+async def main():
+    print("BOT STARTED")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
